715667240b6dba506fdc67cac11b96c103c339b6
mohammedwazier <m.waziruddin@gmail.com>
2023-12-12 21:58:43 +0700

    Updates

--- src/components/Ably.js
+++ src/components/Ably.js
@@ -1,7 +1,8 @@
 import { Realtime } from "ably";
 
 const Alby = () => {
-  return Realtime(process.env.REACT_APP_ABLY_API_KEY);
+  const alby = new Realtime(process.env.REACT_APP_ABLY_API_KEY);
+  return alby;
 };
 
 export default Alby();
